0fb2dafd0125b126a9c4bb053f34b568971fbab0
mranasspro <mranassradi@gmail.com>
2020-02-24 15:02:46 +0100

    Initial commit

---
+++ src/main/java/com/virtuo/gestbancaire/GestionBancaireApplication.java
@@ -0,0 +1,13 @@
+package com.virtuo.gestbancaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionBancaireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionBancaireApplication.class, args);
+    }
+
+}
