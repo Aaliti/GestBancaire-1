43a8549e396a23b2f4fd298562ead22d2e4d3293
Aaliti <61394575+Aaliti@users.noreply.github.com>
2020-02-25 11:05:02 +0100

    solde

--- src/main/java/com/virtuo/gestbancaire/dao/OperationRepository.java
+++ src/main/java/com/virtuo/gestbancaire/dao/OperationRepository.java
@@ -3,7 +3,9 @@ package com.virtuo.gestbancaire.dao;
 import com.virtuo.gestbancaire.entities.Operation;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -22,4 +24,10 @@ public interface OperationRepository extends PagingAndSortingRepository<Operatio
 
     @Override
     Page<Operation> findAll(Pageable pageable);
+
+
+    @Query(nativeQuery = true, value = "select SUM(montant) from operation where compte_id = ?1")
+    public double getSolde(Long id);
+
+
 }
