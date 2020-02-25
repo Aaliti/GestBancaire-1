65681ba504ee2a0cad0e34d81dc9d7c642e80735
Aaliti <61394575+Aaliti@users.noreply.github.com>
2020-02-25 09:56:21 +0100

    view & controller

---
+++ src/main/java/com/virtuo/gestbancaire/controllers/OperationController.java
@@ -0,0 +1,40 @@
+package com.virtuo.gestbancaire.controllers;
+
+
+import com.virtuo.gestbancaire.Services.CompteService;
+import com.virtuo.gestbancaire.entities.Compte;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+
+public class OperationController {
+
+    @Autowired
+    CompteService compServ;
+
+    @GetMapping("/comptes")
+    public String listComptes(Model model){
+
+        List<Compte> comptes = compServ.getAll();
+        model.addAttribute("comptesList" , comptes);
+        return "comptes/list-comptes";
+    }
+
+
+    @GetMapping(path = "/{id}/operations")
+    public String historiqueComptes(@PathVariable("id") long id){
+
+
+        return "comptes/operations";
+    }
+
+
+
+}
